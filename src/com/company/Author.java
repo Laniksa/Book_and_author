bde7c700557fb1b209bf803b2ea98e4922bdd0da
Laniksa <rmashkovtseva@gmail.com>
2023-01-16 19:14:30 +0300

    Накидала пункты меню Примерные методы и их связи

--- src/com/company/Author.java
+++ src/com/company/Author.java
@@ -4,13 +4,20 @@ public class Author {
     private static String name;
     private static String surname;
     private static String nickname;
-    private static String books_written [] =new String[10];
+    private static Book books_written [] =new Book[10];
 
-    Author(String name, String surname, String nickname, String books_written[]){
+    Author(String name, String surname, String nickname, Book books_written[]){
         this.name = name;
         this.surname = surname;
         this.nickname = nickname;
         this.books_written = books_written;
+
+    }
+    Author(String name, String surname, String nickname){
+        this.name = name;
+        this.surname = surname;
+        this.nickname = nickname;
+
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Author addAuthor(Scanner scan){
+        String str;
+        scan.nextLine();
+        System.out.println("Введите имя автора:\n");
+        String nameAuthor = scan.nextLine();
+        System.out.println("Введите фамилию автора:\n");
+        String surnameAuthor = scan.nextLine();
+        System.out.println("Введите псевдоним автора:\n");
+        String nickNameAuthor = scan.nextLine();
+        while(str.equals("end"){
+            System.out.println("Введите названия книг автора:\n");
+            //отдельный метод по добавлению книг
+        }
+
+
+
+        return new Author (nameAuthor,surnameAuthor,nickNameAuthor);
+    }
+    Scanner scan = new Scanner(System.in);
+    while(true){
+        String num = scan.nextLine();
+        System.out.println("Введите пункт меню:\n" +
+                "1. Добавить автора\n" +
+                "2. Добавить книгу\n" +
+                "3. Показать всех авторов\n" +
+                "4. Показать все книги\n"
+        );
+
+        switch (num){
+            case 1:
+                addAuthor(Scanner scan);
+                break;
+
+        }
+    }
 
     public static void main(String[] args) {
 	// write your code here
